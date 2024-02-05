230bd6c75423eb4bc8ce640269bb04b2017aa623
Kosmak07 <159026685+Kosmak07@users.noreply.github.com>
2024-02-05 20:57:48 +0500

    CameraScript

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public Transform CameraAxisTransform;
+
+    public float RotationSpeed;
+    public int minAngle;
+    public int maxAngle;
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+        
+        var newAngle = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngle > 180)
+        {
+            newAngle -= 360;
+        }
+        
+        newAngle = Mathf.Clamp(newAngle, minAngle, maxAngle);
+        
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngle, 0, 0);
+    }
+}
